7aba5552b749b6c0a3a424638b8063b0708c8c6a
Sergio <sopekof@gmail.com>
2025-06-12 22:05:28 -0300

    Clase 10 - Api Rest

--- Clases/Clase-10/index.js
+++ Clases/Clase-10/index.js
@@ -2,6 +2,21 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const productos = [
+  { id: 1, nombre: "Producto 1", precio: 100 },
+  { id: 2, nombre: "Producto 2", precio: 200 },
+];
 
+app.get("/", (req, res) => {
+  res.send("<h1> Hola desde la API Rest, nodemon <h1>");
+});
+
+app.get("/productos/:id", (req, res) => {
+  const productosId = productos.find(
+    (item) => item.id === parseInt(req.params.id),
+  );
+  res.json(productosId);
+});
+
+const PORT = 3000;
 app.listen(PORT, () => console.log("Servidor corriendo en Puerto: " + PORT));
