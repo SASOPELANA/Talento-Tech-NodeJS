06ff6beaf11b768258d50f1f90019b106920d904
Sergio Devs <sopekof@gmail.com>
2025-07-08 15:53:18 -0300

    Clase 13

--- Clases/Clase-13/src/models/products.model.js
+++ Clases/Clase-13/src/models/products.model.js
@@ -1,3 +1,6 @@
+// Aquí no responde el modelo, responde el controlador
+// El controlador responde al cliente, aquí solo se devuelve datos lógicos
+
 import fs from "fs";
 
 import path from "path";
@@ -10,10 +13,42 @@ const json = fs.readFileSync(jsonPath, "utf-8");
 
 const products = JSON.parse(json);
 
+// console.log(products);
+
+// get all
 export const getAllProducts = () => {
 	return products;
 };
 
-export const getAllProductBy = (id) => {
+// get id
+export const getProductById = (id) => {
 	return products.find((item) => item.id === id);
+};
+
+// post
+export const postProduct = (data) => {
+	const newProduct = {
+		id: products.length + 1,
+		...data,
+	};
+
+	products.push(newProduct);
+	// Escribir en el json o archivo
+	fs.writeFileSync(jsonPath, JSON.stringify(products));
+	return newProduct;
+};
+
+// Delete
+export const deleteProductId = (id) => {
+	const productIndex = products.findIndex((p) => p.id === id);
+
+	if (productIndex != -1) {
+		const product = products.splice(productIndex, 1);
+
+		fs.writeFileSync(jsonPath, JSON.stringify(products));
+
+		return product;
+	} else {
+		return null;
+	}
 };
--- Clases/Clase-13/src/services/products.service.js
+++ Clases/Clase-13/src/services/products.service.js
@@ -5,3 +5,7 @@ import * as model from "../models/products.model.js";
 export const getAllProducts = () => {
 	return model.getAllProducts();
 };
+
+export const getProductById = (id) => {
+	return model.getProductById(id);
+};
